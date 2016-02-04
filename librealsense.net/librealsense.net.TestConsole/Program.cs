ba237effadea0b9f932070b9c3f1de387eb41ab8
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-02-04 17:13:28 +0900

    ハンドルごとに内部クラスに変更

--- librealsense.net/librealsense.net.TestConsole/Program.cs
+++ librealsense.net/librealsense.net.TestConsole/Program.cs
@@ -9,6 +9,20 @@ namespace librealsense.net.TestConsole
     {
         static void Main( string[] args )
         {
+            IntPtr error = IntPtr.Zero;
+            var ctx = NativeMethod.Context.rs_create_context(4, error );
+
+            var count = NativeMethod.Context.rs_get_device_count(ctx, error);
+            Console.WriteLine(count);
+
+            if ( count > 0 ) {
+                var device = NativeMethod.Context.rs_get_device( ctx, 0, error );
+                var deviceName = NativeMethod.Device.rs_get_device_name(device, error);
+                Console.WriteLine( deviceName );
+            }
+
+            NativeMethod.Context.rs_delete_context( ctx, error );
+
         }
     }
 }
--- librealsense.net/librealsense.net/NativeMethod.cs
+++ librealsense.net/librealsense.net/NativeMethod.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace librealsense
 {
     public static class NativeMethod
     {
+        //static string DllPath = ((IntPtr.Size == 4) ? @"lib\x86\" : @"lib\x64\") + "realsense.dll";
+
+        const string DllPath = @"lib\x86\realsense.dll";
+
+        public static class Context
+        {
+            [DllImport( DllPath )]
+            public static extern IntPtr rs_create_context( int api_version, IntPtr error );
+
+            [DllImport( DllPath )]
+            public static extern void rs_delete_context( IntPtr context, IntPtr error );
+
+            [DllImport( DllPath )]
+            public static extern int rs_get_device_count( IntPtr context, IntPtr error );
+
+            [DllImport( DllPath )]
+            public static extern IntPtr rs_get_device( IntPtr context, int index, IntPtr error );
+        }
+
+
+        public static class Device
+        {
+            [DllImport( DllPath )]
+            public static extern string rs_get_device_name( IntPtr device, IntPtr error );
+        }
     }
 }
